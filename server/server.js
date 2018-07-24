60b6bd3dc3566fd71cd4396adaa0e42465085d80
Richard Norris <richardnorris@Richards-MacBook-Pro.local>
2018-07-24 17:12:30 +0200

    Add GET todos/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -28,6 +29,45 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     });
 });
+
+// GET /todos/123455
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id; 
+
+    // res.send(req.params);
+
+    // valid id using isvalid
+
+    if (!ObjectID.isValid(id)) {
+            // console.log('ID not valid');
+              // 404 - send back empty body
+            return res.status(404).send();
+        }
+
+        Todo.findById(id).then((todo) => {
+                if(!todo){
+                    // return console.log('Id not found');
+                    return res.status(404).send();
+                }
+                    res.send({todo});
+            //     console.log('Todo By Id', todo);
+            // }).catch((e) => console.log());
+        }).catch((e) => {res.status(400).send()
+            });
+
+
+  
+
+
+
+    //findByID
+        //success
+            // if todo - send back
+            // if no todo - send back 404 with empty body
+        //error
+            //400 - and send empty body back
+});
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
